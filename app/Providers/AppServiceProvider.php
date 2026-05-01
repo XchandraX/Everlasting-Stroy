aa10a1feed392f3a3b41be275c87cc694d3d03df
XchandraX <XchandraX@users.noreply.github.com>
2026-05-01 17:57:08 +0700

    setting hosting vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,10 +17,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot() {
-    if (env('APP_ENV') === 'production') {
-        \URL::forceScheme('https');
-        $this->app['request']->server->set('HTTPS', 'on');
+    public function boot()
+    {
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
-}
 }
